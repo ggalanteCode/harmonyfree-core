26060044050d5204912dd0e23df7d51452d4ad04
ggalanteCode <ggalante3@gmail.com>
2026-05-19 12:53:14 +0200

    initial commit

---
+++ src/main/java/com/generation153/harmonyfree/core/HarmonyfreeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.generation153.harmonyfree.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyfreeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyfreeCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation153/harmonyfree/core/HarmonyfreeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation153.harmonyfree.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyfreeCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
