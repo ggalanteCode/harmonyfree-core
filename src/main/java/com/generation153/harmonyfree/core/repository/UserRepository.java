4d7677866059db53ec18770b92d9e5d8eca08891
Stefano Rimenti <email@studenti.com>
2026-05-20 17:05:03 +0200

    UserRepository extends JpaRepository

--- src/main/java/com/generation153/harmonyfree/core/repository/UserRepository.java
+++ src/main/java/com/generation153/harmonyfree/core/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.generation153.harmonyfree.core.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.generation153.harmonyfree.core.entity.User;
 
-public interface UserRepository  {
+public interface UserRepository  extends JpaRepository<User , Long> {
 
 }
