6393d65e57453770ef3d8bc6e1a98e317318f1d2
ggalanteCode <ggalante3@gmail.com>
2026-05-20 14:42:30 +0200

    aggiunto TrackRepository

---
+++ src/main/java/com/generation153/harmonyfree/core/repository/TrackRepository.java
@@ -0,0 +1,7 @@
+package com.generation153.harmonyfree.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation153.harmonyfree.core.entity.Track;
+
+public interface TrackRepository extends JpaRepository<Track, Long> {}
