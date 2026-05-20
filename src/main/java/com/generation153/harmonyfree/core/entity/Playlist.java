3557d5890d8d5f4e337d87e2a72306d81182958d
raphadevilla12 <raphadevilla12@gmail.com>
2026-05-20 16:25:50 +0200

    cambio playlists in playlist

--- src/main/java/com/generation153/harmonyfree/core/entity/Playlist.java
+++ src/main/java/com/generation153/harmonyfree/core/entity/Playlist.java
@@ -51,7 +51,7 @@ public class Playlist {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "playlists", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PlaylistTrack> playlistTracks = new ArrayList<>();
 
 }
