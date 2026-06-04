1a00297a1e219fb3b48ccbc2af35fdfe65fa1603
ggalanteCode <ggalante3@gmail.com>
2026-06-04 10:54:25 +0200

    releasedate in descending order in case of searchTracks without filters, and retrieve the first 50 results.

--- src/main/java/com/generation153/harmonyfree/core/client/JamendoClient.java
+++ src/main/java/com/generation153/harmonyfree/core/client/JamendoClient.java
@@ -28,25 +28,46 @@ public class JamendoClient {
     	
     	log.info("Jamendo clientId: " + clientId);
     	
-    	return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/tracks")
-                        .queryParam("client_id", clientId)
-                        .queryParam("format", "json")
-                        .queryParam("limit", Math.min(request.getLimit(), 200))
-                        .queryParam("offset", request.getOffset())
-                        .queryParamIfPresent("search", Optional.ofNullable(request.getQuery()))
-                        .queryParamIfPresent("name", Optional.ofNullable(request.getTitle()))
-                        .queryParamIfPresent("artist_name", Optional.ofNullable(request.getArtist()))
-                        .queryParamIfPresent("album_name", Optional.ofNullable(request.getAlbum()))
-                        .queryParamIfPresent("tags", Optional.ofNullable(request.getGenre()))
-                        .queryParam("include", "musicinfo")
-                        .build())
+    	boolean hasFilters = searchHasFilters(request);
+
+        return webClient.get()
+                .uri(uriBuilder -> {
+
+                    var builder = uriBuilder
+                            .path("/tracks")
+                            .queryParam("client_id", clientId)
+                            .queryParam("format", "json")
+                            .queryParam("limit", Math.min(request.getLimit(), 200))
+                            .queryParam("offset", request.getOffset())
+                            .queryParamIfPresent("search", Optional.ofNullable(request.getQuery()))
+                            .queryParamIfPresent("name", Optional.ofNullable(request.getTitle()))
+                            .queryParamIfPresent("artist_name", Optional.ofNullable(request.getArtist()))
+                            .queryParamIfPresent("album_name", Optional.ofNullable(request.getAlbum()))
+                            .queryParamIfPresent("tags", Optional.ofNullable(request.getGenre()))
+                            .queryParam("include", "musicinfo");
+
+                    //se la search non ha dei filtri di ricerca, 
+                    //ordina i risultati per data di pubblicazione decrescente, 
+                    //altrimenti mantieni l'ordinamento di default, ovvero per "rilevanza" di Jamendo
+                    if (!hasFilters) {
+                        builder.queryParam("order", "releasedate_desc");
+                    }
+
+                    return builder.build();
+                })
                 .retrieve()
                 .bodyToMono(JamendoSearchResponse.class)
                 .block();
     	
     }
+
+	private boolean searchHasFilters(TrackSearchRequest request) {
+		return request.getQuery() != null ||
+                request.getTitle() != null ||
+                request.getArtist() != null ||
+                request.getAlbum() != null ||
+                request.getGenre() != null;
+	}
 
 	public JamendoTrackResponse getTrackById(Long jamendoTrackId) {
 		
